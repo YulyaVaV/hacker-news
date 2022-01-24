acab3ae9654b4c61821dada78d5e497f353ad7e5
unknown <valchukyu@gmail.comm>
2022-01-24 17:08:33 +0200

    Solution

---
+++ src/components/Header.vue
@@ -0,0 +1,13 @@
+<template>
+  <header class="header">
+    <img src="../assets/images/icon.png" alt="hacker news" class="header__icon">
+    <a href="/" class="header__title">Hacker News</a>
+  </header>
+</template>
+
+<script>
+import '../assets/styles/Header.scss';
+export default {
+  name: "Header"
+}
+</script>
